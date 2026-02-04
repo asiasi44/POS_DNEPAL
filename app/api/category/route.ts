7a8596113b0b6d09e048d99379847cd78a1309b9
Jubing01 <np03cs4s220194@heraldcollege.edu.np>
2026-02-05 01:15:44 +0545

    feat(route changes): json error

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -7,41 +7,41 @@ import slugify from "slugify";
 type Params = { params: Promise<{ id: string }> };
 
 export const POST = withErrorHandler<Params>(async function (
-  request: NextRequest,
-  { params }: Params
+	request: NextRequest,
+	{ params }: Params,
 ) {
-  const body = await request.json();
+	const body = await request.json();
 
-  const slug = slugify(body.name, {
-    lower: true,
-  });
+	const slug = slugify(body.name, {
+		lower: true,
+	});
 
-  const user = await verifyAuth();
+	const user = await verifyAuth();
 
-  const createdUser = user.role;
+	const createdUser = user.role ;
 
-  const createdCategory = await prisma.category.create({
-    data: {
-      name: body.name,
-      createdBy: createdUser,
-      slug: slug,
-    },
-  });
+	const createdCategory = await prisma.category.create({
+		data: {
+			name: body.name,
+			createdBy: createdUser,
+			slug: slug,
+		},
+	});
 
-  return NextResponse.json({
-    success: true,
-    updatedCategories: createdCategory,
-  });
+	return NextResponse.json({
+		success: true,
+		categories: createdCategory,
+	});
 });
 
 export const GET = withErrorHandler<Params>(async function (
-  request: NextRequest,
-  { params }: Params
+	request: NextRequest,
+	{ params }: Params,
 ) {
-  const allCategories = await prisma.category.findMany();
+	const allCategories = await prisma.category.findMany();
 
-  return NextResponse.json({
-    success: true,
-    categories: allCategories,
-  });
+	return NextResponse.json({
+		success: true,
+		categories: allCategories,
+	});
 });
