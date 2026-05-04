4fa50125a5f0d98fa840d99b1cfb3b7ac598f48b
Asim Poudel <bipulp49@gmail.com>
2026-05-04 12:16:27 +0545

    fix: use neon prisma in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,33 @@
-import { PrismaClient, UserRole } from "@/app/generated/prisma/client";
+// import { PrismaClient, UserRole } from "@/app/generated/prisma/client";
+// import bcrypt from "bcryptjs";
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   const existing = await prisma.user.findFirst({
+//     where: { role: UserRole.SUPER_ADMIN },
+//   });
+
+//   if (existing) return;
+
+//   await prisma.user.create({
+//     data: {
+//       email: "superadmin@gmail.com",
+//       password: await bcrypt.hash("Superadmin", 10),
+//       role: UserRole.SUPER_ADMIN,
+//       isActive: true,
+//     },
+//   });
+
+//   console.log("✅ Super admin seeded");
+// }
+
+// main()
+//   .catch(console.error)
+//   .finally(() => prisma.$disconnect());
+
+import { PrismaClient } from "@prisma/client";
+import { UserRole } from "@/app/generated/prisma/enums";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
